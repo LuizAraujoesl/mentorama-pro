2597c97a0352253b2c2ea87a140cff1999ff54a7
Luizesl14 <luiz.develop@gmail.com>
2021-06-06 00:33:54 -0300

    Mapeando Class MensagemController

--- EXERCICIO-MOD 03/src/main/java/com/mentorama/exerciciomod03/MensagemController.java
+++ EXERCICIO-MOD 03/src/main/java/com/mentorama/exerciciomod03/MensagemController.java
@@ -1,5 +1,16 @@
 package com.mentorama.exerciciomod03;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MensagemController {
+
+    @GetMapping
+    public String msg (){
+        String mensagem = "Ola mundo!";
+        return mensagem;
+    }
 
 }
