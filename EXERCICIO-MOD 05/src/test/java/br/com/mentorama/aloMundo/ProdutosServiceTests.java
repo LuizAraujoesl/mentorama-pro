59da86beb0ca12857a3c2d7ba9f99c8d3182cbe6
Luizesl <luiz@bluetechno.com.br>
2021-10-23 17:26:03 -0300

    Testes de integracao finalizados

--- EXERCICIO-MOD 05/src/test/java/br/com/mentorama/aloMundo/ProdutosServiceTests.java
+++ EXERCICIO-MOD 05/src/test/java/br/com/mentorama/aloMundo/ProdutosServiceTests.java
@@ -4,13 +4,19 @@ import br.com.mentorama.aloMundo.entities.Cesta;
 import br.com.mentorama.aloMundo.entities.ItensCesta;
 import br.com.mentorama.aloMundo.entities.Produtos;
 import br.com.mentorama.aloMundo.repositories.ProdutosRepository;
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
+
 @SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class ProdutosServiceTests {
 
 
@@ -135,6 +141,48 @@ class ProdutosServiceTests {
 		}
 
 		Assertions.assertEquals(17.5, cesta.getDescount());
+	}
+
+	private  static final String MOCKED_RESULT =
+			            "   {\n" +
+						"   [\n" +
+						"   {\n" +
+		                "   \"id\": 1,\n" +
+						"   \"type\": \"eletrônicos\", \n" +
+						"   \"name\": \"celular_xiaomi\",\n" +
+						"   \"price\": 999.0," +
+						"   \"amount\": 7 \n" +
+			            "     } \n"+
+					    "     ]\n" +
+
+				        "     }\n";
+
+	private  static WireMockServer wireMockServer =  new WireMockServer(options().port(8081));
+
+	@BeforeAll
+	static void berforAll(){
+		wireMockServer.start();
+	}
+
+	@BeforeEach
+	void setUp(){
+		wireMockServer.resetAll();
+	}
+
+
+	@Test
+	@DisplayName("Tente de Integracao")
+	public void shouldFindAllProducts() throws Exception{
+		wireMockServer.stubFor(WireMock.get(WireMock.urlEqualTo("/loja/produtos"))
+				.willReturn(aResponse()
+						.withStatus(200)
+						.withHeader("Content-Type", "application/json")
+						.withBody(MOCKED_RESULT)));
+	}
+
+	@AfterAll
+	void staticAll(){
+		wireMockServer.stop();
 	}
 
 }
