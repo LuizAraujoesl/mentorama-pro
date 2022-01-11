902ecc308bb069db13d46ed3ca833b9da098faed
Luizesl <luiz@bluetechno.com.br>
2022-01-11 00:26:13 -0300

    Iniciando exercici

--- EXERCICIO/src/main/java/com/systemhospital/repository/ProfessionalMedicineRepository.java
+++
@@ -1,7 +0,0 @@
-package com.systemhospital.repository;
-
-import com.systemhospital.entities.ProfessionalMedicine;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProfessionalMedicineRepository extends JpaRepository<ProfessionalMedicine, Integer> {
-}
