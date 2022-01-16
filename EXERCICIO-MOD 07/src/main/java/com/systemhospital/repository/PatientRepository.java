5d7aea2ab62e38ba76d2ae275778f16f002a95af
Luiz Augusto <luiz.develop@gmail.com>
2022-01-16 14:37:10 -0300

    Delete Ex 07
    Organizando Pastas

--- EXERCICIO-MOD 07/src/main/java/com/systemhospital/repository/PatientRepository.java
+++
@@ -1,7 +0,0 @@
-package com.systemhospital.repository;
-
-import com.systemhospital.entities.Patient;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PatientRepository extends JpaRepository<Patient, Integer> {
-}
